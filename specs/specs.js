16d36eff621d75ecafd6928b125b42385cf4d308
Will Murphy <wilmurph@gmail.com>
2016-01-29 08:26:34 -0800

    add and fail first PizzaOrder constuctor spec

--- specs/specs.js
+++ specs/specs.js
@@ -1,4 +1,13 @@
-describe('pingPong', function() {
-  it("will count up by 1 to input number", function() {
-    expect(pingPong(2)).to.eql([1, 2]);
+describe('PizzaOrder', function() {
+  it("will take a new pizza order with the given properties", function() {
+    var testPizzaOrder = new PizzaOrder;
+    expect(testPizzaOrder.smallPizza).to.equal(6);
+    expect(testPizzaOrder.mediumPizza).to.equal(9);
+    expect(testPizzaOrder.largePizza).to.equal(12);
+    expect(testPizzaOrder.pepperoni).to.equal(2);
+    expect(testPizzaOrder.anchovies).to.equal(4);
+    expect(testPizzaOrder.veggie).to.equal(3);
+    expect(testPizzaOrder.olives).to.equal(1);
+    expect(testPizzaOrder.pineapple).to.equal(2);
+  });
 });
